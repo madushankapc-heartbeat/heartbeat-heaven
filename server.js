7bc4e6a95e8381021a550c5179bfb033328e6072
madushankapc-heartbeat <madushankapc@gmail.com>
2026-09-07 13:17:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -123,5 +123,5 @@ app.delete("/api/songs/:id", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+app.get("/{*splat}", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 app.listen(PORT, () => console.log(`Heartbeat Heaven running on port ${PORT}`));
