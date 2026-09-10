d7a9dc43c151d61de4fb2678355d216182b38861
madushankapc-heartbeat <madushankapc@gmail.com>
2026-09-10 12:14:58 +0530

    refactor: remove startup source compiler

--- startup-fix.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const Module = require("module");
-
-const visitorPath = path.resolve(__dirname, "visitor-server.js");
-
-const visitorSource = fs.readFileSync(visitorPath, "utf8");
-
-module._compile(visitorSource, visitorPath);
