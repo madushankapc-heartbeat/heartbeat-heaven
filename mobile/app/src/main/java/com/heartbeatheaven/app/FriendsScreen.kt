6c84108ff0644403e4f876b180359b351699d0f7
madushankapc-heartbeat <madushankapc@gmail.com>
2026-09-20 11:31:30 +0530

    Fix swipe-to-reply pointer input import

--- mobile/app/src/main/java/com/heartbeatheaven/app/FriendsScreen.kt
+++ mobile/app/src/main/java/com/heartbeatheaven/app/FriendsScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import coil.compose.AsyncImage
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.window.Dialog
