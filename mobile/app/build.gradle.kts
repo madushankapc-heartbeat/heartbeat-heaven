b0d0ca827ebb21d926ca27affe07358af3a57372
madushankapc-heartbeat <madushankapc@gmail.com>
2026-09-21 07:56:40 +0530

    Remove retired WebRTC dependency

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -212,7 +212,6 @@ dependencies {
     implementation("androidx.media3:media3-transformer:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("io.github.webrtc-sdk:android:150.7871.01")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
